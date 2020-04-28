9a71802824165a04d8cfba52c93267c8434fedc4
Chafro <ChafroBrown@gmail.com>
2020-04-27 23:28:59 -0500

    added upload form

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,11 @@
 
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField,TextAreaField
+from wtforms.validators import DataRequired, Email
+
+
+
+class UploadForm(FlaskForm):
+	description = TextAreaField('Description', validators=[DataRequired()])
+	file = FileField('Photo', validators=[FileRequired(),FileAllowed(['jpg', 'png', 'jpeg'])])
